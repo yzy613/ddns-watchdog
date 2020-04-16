11b5df7d6f3b60040ab4ebeeb0f66b0f8a9fe0b9
yzy613 <59520517+yzy613@users.noreply.github.com>
2020-04-16 17:05:12 +0800

    修复发布问题

--- build.sh
+++ build.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # server
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o ddns-server ./ddns-server.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o ddns-server ./main-code/server/ddns-server.go
 tar -czvf linux_amd64_server.tar.gz ddns-server
 rm -f ddns-server
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o ddns-server.exe ./ddns-server.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o ddns-server.exe ./main-code/server/ddns-server.go
 tar -czvf windows_amd64_server.tar.gz ddns-server.exe
 rm -f ddns-server.exe
 
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o ddns-server ./ddns-server.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o ddns-server ./main-code/server/ddns-server.go
 tar -czvf linux_arm64_server.tar.gz ddns-server
 rm -f ddns-server
 
 # client
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o ddns-client ./ddns-client.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o ddns-client ./main-code/client/ddns-client.go
 tar -czvf linux_amd64_client.tar.gz ddns-client
 rm -f ddns-client
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o ddns-client.exe ./ddns-client.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-w -s" -o ddns-client.exe ./main-code/client/ddns-client.go
 tar -czvf windows_amd64_client.tar.gz ddns-client.exe
 rm -f ddns-client.exe
 
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o ddns-client ./ddns-client.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o ddns-client ./main-code/client/ddns-client.go
 tar -czvf linux_arm64_client.tar.gz ddns-client
 rm -f ddns-client
