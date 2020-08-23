2923a3fb738634eb2166541112aa4d174d5725c9
yzy <59520517+yzy613@users.noreply.github.com>
2020-08-23 09:54:59 +0800

    v1.2.2 f: Update batch

--- watchdog-ddns-client-startup-script.bat
+++ watchdog-ddns-client-startup-script.bat
@@ -4,10 +4,10 @@ title=watchdog-ddns-client-startup-script
 color 3f
 
 SET conf=.\conf
-if not exist %conf% (call watchdog-ddns-client -init)else goto start
-echo.
-echo Program Init.
+if not exist %conf% (call watchdog-ddns-client -init) else goto start
+echo watchdog-ddns-client has initialized
 echo Please change .\conf\client.json
+echo.
 echo Press any key to contiune ...
 pause>nul
 cls
@@ -22,7 +22,8 @@ echo =================================================================
 echo.
 call watchdog-ddns-client.exe
 echo.
-echo Program exit. Press any key to restart watchdog-ddns ...Fix
+echo watchdog-ddns-client has exited.
+echo Press any key to restart watchdog-ddns ...
 pause>nul
 cls
 goto start
