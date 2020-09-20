981605e2d48af436ad886ea3c1a7203fa4cd983f
yzy613 <59520517+yzy613@users.noreply.github.com>
2020-09-20 09:00:15 +0800

    v1.3.0 d: 优化 build

--- build.sh
+++ build.sh
@@ -6,7 +6,12 @@ SERVER_NAME='watchdog-ddns-server'
 SERVER_CODE_FILE='./main-code/server/watchdog-ddns-server.go'
 OUTPUT_PATH='./build/'
 
+# check if the $OUTPUT_PATH folder exists
+if [ ! -d "$OUTPUT_PATH" ]; then
+    mkdir "$OUTPUT_PATH"
+fi
 
+# start building
 # client
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o $OUTPUT_PATH$CLIENT_NAME $CLIENT_CODE_FILE
 tar -czvf $OUTPUT_PATH$CLIENT_NAME.linux-amd64.tar.gz -C $OUTPUT_PATH $CLIENT_NAME
@@ -18,6 +23,15 @@ rm -f $OUTPUT_PATH$CLIENT_NAME.exe
 
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-w -s" -o $OUTPUT_PATH$CLIENT_NAME $CLIENT_CODE_FILE
 tar -czvf $OUTPUT_PATH$CLIENT_NAME.darwin-amd64.tar.gz -C $OUTPUT_PATH $CLIENT_NAME
+rm -f $OUTPUT_PATH$CLIENT_NAME
+
+# arm
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o $OUTPUT_PATH$CLIENT_NAME $CLIENT_CODE_FILE
+tar -czvf $OUTPUT_PATH$CLIENT_NAME.linux-arm64.tar.gz -C $OUTPUT_PATH $CLIENT_NAME
+rm -f $OUTPUT_PATH$CLIENT_NAME
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-w -s" -o $OUTPUT_PATH$CLIENT_NAME $CLIENT_CODE_FILE
+tar -czvf $OUTPUT_PATH$CLIENT_NAME.linux-arm_v7.tar.gz -C $OUTPUT_PATH $CLIENT_NAME
 rm -f $OUTPUT_PATH$CLIENT_NAME
 
 # mips
@@ -37,15 +51,6 @@ CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "-w -s" -o $OUTPUT_PATH$C
 tar -czvf $OUTPUT_PATH$CLIENT_NAME.linux-mips.tar.gz -C $OUTPUT_PATH $CLIENT_NAME
 rm -f $OUTPUT_PATH$CLIENT_NAME
 
-# arm
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o $OUTPUT_PATH$CLIENT_NAME $CLIENT_CODE_FILE
-tar -czvf $OUTPUT_PATH$CLIENT_NAME.linux-arm64.tar.gz -C $OUTPUT_PATH $CLIENT_NAME
-rm -f $OUTPUT_PATH$CLIENT_NAME
-
-CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-w -s" -o $OUTPUT_PATH$CLIENT_NAME $CLIENT_CODE_FILE
-tar -czvf $OUTPUT_PATH$CLIENT_NAME.linux-arm_v7.tar.gz -C $OUTPUT_PATH $CLIENT_NAME
-rm -f $OUTPUT_PATH$CLIENT_NAME
-
 
 # server
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-w -s" -o $OUTPUT_PATH$SERVER_NAME $SERVER_CODE_FILE
@@ -58,6 +63,15 @@ rm -f $OUTPUT_PATH$SERVER_NAME.exe
 
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-w -s" -o $OUTPUT_PATH$SERVER_NAME $SERVER_CODE_FILE
 tar -czvf $OUTPUT_PATH$SERVER_NAME.darwin-amd64.tar.gz -C $OUTPUT_PATH $SERVER_NAME
+rm -f $OUTPUT_PATH$SERVER_NAME
+
+# arm
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o $OUTPUT_PATH$SERVER_NAME $SERVER_CODE_FILE
+tar -czvf $OUTPUT_PATH$SERVER_NAME.linux-arm64.tar.gz -C $OUTPUT_PATH $SERVER_NAME
+rm -f $OUTPUT_PATH$SERVER_NAME
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-w -s" -o $OUTPUT_PATH$SERVER_NAME $SERVER_CODE_FILE
+tar -czvf $OUTPUT_PATH$SERVER_NAME.linux-arm_v7.tar.gz -C $OUTPUT_PATH $SERVER_NAME
 rm -f $OUTPUT_PATH$SERVER_NAME
 
 # mips
@@ -75,13 +89,4 @@ rm -f $OUTPUT_PATH$SERVER_NAME
 
 CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "-w -s" -o $OUTPUT_PATH$SERVER_NAME $SERVER_CODE_FILE
 tar -czvf $OUTPUT_PATH$SERVER_NAME.linux-mips.tar.gz -C $OUTPUT_PATH $SERVER_NAME
-rm -f $OUTPUT_PATH$SERVER_NAME
-
-# arm
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-w -s" -o $OUTPUT_PATH$SERVER_NAME $SERVER_CODE_FILE
-tar -czvf $OUTPUT_PATH$SERVER_NAME.linux-arm64.tar.gz -C $OUTPUT_PATH $SERVER_NAME
-rm -f $OUTPUT_PATH$SERVER_NAME
-
-CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-w -s" -o $OUTPUT_PATH$SERVER_NAME $SERVER_CODE_FILE
-tar -czvf $OUTPUT_PATH$SERVER_NAME.linux-arm_v7.tar.gz -C $OUTPUT_PATH $SERVER_NAME
 rm -f $OUTPUT_PATH$SERVER_NAME
